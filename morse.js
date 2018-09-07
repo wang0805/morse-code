62b002bf691aa1178b4aad67882231368fba7480
wh_wang@Live.com <wh_wang@live.com>
2018-09-07 11:52:35 +0800

    encode and decode

--- morse.js
+++ morse.js
@@ -27,3 +27,63 @@ var alphabet = {
   y: '-.--',
   z: '--..'
 };
+
+var swapAlph = swapAlp(alphabet);
+
+var firstWord = "hello world";
+
+var secondWord = "we love javascript";
+
+function encode(a){
+  var newArray =[];
+  for (var i=0;i<a.length;i++){
+    for (alp in alphabet){
+      if (a.charAt(i) == alp){
+        newArray.push(alphabet[alp]);
+      }
+    }
+    if (a.charAt(i) == " "){
+      newArray.push(a.charAt(i));
+    }
+  }
+  return newArray;
+}
+
+var userInput = prompt("enter a string");
+
+console.log(encode(userInput));
+
+need to do function of finding key using value (below is wrong)
+
+function findKey (obj,value){
+  return Object.keys(obj).find(key => obj[key] === value);
+}
+
+function swapAlp(a){
+  var newObj = {};
+  for (alp in alphabet){
+    newObj[a[alp]] = alp;
+  }
+  return newObj;
+}
+
+function decode(b){
+  var newArray = [];
+  var j = 0;
+  for (var i = 0;i<b.length;i++){
+    if(b.charAt(i)==" "){
+      var morse = b.substring(j,i);
+      j = i+1;
+      for (a in swapAlph){
+        if(morse === a){
+          newArray.push(swapAlph[a]);
+        }
+      }
+    }
+  }
+  newArray.push(swapAlph[b.substring(j,b.length)]);
+  var str = newArray.toString()
+  return str;
+}
+
+console.log(decode('... .- -- ..- . .-..    -- --- .-. ... .    .. -. ...- . -. - . -..    -- --- .-. ... .    -.-. --- -.. .'));
